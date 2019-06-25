38c86d1bf051ac6c00a169501ce95c6e30dc7fe6
Akanksha <manishsctce@gmail.com>
2019-06-25 07:36:44 +0530

    deleted corejava-bin

--- CoreJava/src/polymorphism/MethodOverloadingDemo1.java
+++ CoreJava/src/polymorphism/MethodOverloadingDemo1.java
@@ -1,4 +1,5 @@
 package polymorphism;
+
 /*METHOD OVERLOADING
  * Return type doesnot include in method signature
  * Access specifier also doesnot effect method overloading 
@@ -6,67 +7,100 @@ package polymorphism;
  * 
  * varargs method of Java5 implemented
  */
- class Summation{
-	//int side;
-	Summation(){		}
-	
-	/*public int sum(int a, int b){
-		System.out.println("int - int : "+(a+b));
-		return (a+b);
-	}*/
-	
-	// Return type doesnot include in method signature
-	/*public float sum(int a, int b){
-		return (a+b);
-	}*/
-	public int sum(int ...varargs){
+class Summation {
+	// int side;
+	Summation() {
+	}
+
+	/*
+	 * public int sum(int a, int b){ System.out.println("int - int : "+(a+b));
+	 * return (a+b); }
+	 */
+
+	// Return type doesnot include in method signature for method overloading
+	/*
+	 * public float sum(int a, int b){ return (a+b); }
+	 */
+
+	public void multipleVarAgrsMethod(String arp[], int[] a, String... str) {
+
+	}
+
+	// OVERLOADED METHODs
+	public int sum(int... varargs) {
 		System.out.println("--------Varargs----------");
 		int sum = 0;
-		for(int i : varargs)
+		for (int i : varargs)
 			sum = sum + i;
 		return sum;
 	}
-	public void multipleVarAgrsMethod(String arp[], int[] a, String...str){
-		
-	}
-	public float sum(int a, float b){
-		System.out.println("int - float" + (a+b));
-		return (a+b);
+
+	public int sum(String a, int... varargs) {
+		int sum = 0;
+		for (int i : varargs)
+			sum = sum + i;
+		System.out.println("Varargs sum: "+ sum + " String : "+a);
+		return sum;
 	}
 	
-	public double sum(float a, int b){
-		System.out.println("float - int :"+(a+b));//6.23
+	// OVERLOADED METHOD - 1,3.0
+	public float sum(int a, float b) {
+		System.out.println("int - float:" + (a + b));
+		return (a + b);
+	}
+
+	// OVERLOADED METHOD - 1,3.0
+	public double sum(int a, double b) {
+		System.out.println("int - double:" + (a + b));
+		return (a + b);
+	}
+
+	// OVERLOADED METHOD
+	public double sum(float a, int b) {
+		System.out.println("float - int :" + (a + b));// 6.23
 		return a + b;
 	}
-	
-	private int sum (int a, float b, int c){
+
+	// OVERLOADED METHOD
+	private int sum(int a, float b, int c) {
 		return 0;
 	}
-	
-	public static double sum(double a, float f){
-		return 0.0;
+
+	// OVERLOADED METHOD
+	public static double sum(double a, float f) {
+		return a + f;
 	}
-	
-	public final double sum(float f,double d){
-		return 0.0;
+
+	// OVERLOADED METHOD
+	public final double sum(float f, double d) {
+		return f + d;
 	}
-	//public  double sum(float f,double d);
+	// public double sum(float f,double d);
 }
 
 public class MethodOverloadingDemo1 {
-	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+	public static void main(String[] args)
+			throws InstantiationException, IllegalAccessException, ClassNotFoundException {
 		Summation sum = new Summation();
 		
+		//sum.sum(5, 10);//CTE  sum(int[]) ambiguous method
+		
+		sum.sum("mani",5, 10); //Varargs sum: 15 String : mani
+		
+		sum.sum(1, 1.2f);//int-float:2.2
+		sum.sum(1, 2.9);//int-double:3.9
+		sum.sum(1, 3.9d);//int-double:4.9
+
+		// CTE sum(int, int) in the type Summation is not applicable for the args (double, double)
+		//sum.sum(7.3, 8.9);
+		
+
 		Summation sum2 = (Summation) Class.forName("polymorphism.Summation").newInstance();
-		
-		
-		
-		//sum.sum(7.3, 8.9);//Compile-time error sum(int, int) in the type Summation is not applicable for the arguments (double, double)
-		sum.sum(5, 10);
-		
-	//	System.out.println(sum.sum(5, 10,12,34));;
-		float a = (float)sum.sum(6.2f, 3);
-		System.out.println(sum2.sum(5, 10,12,34));;
+
+		// System.out.println(sum.sum(5, 10,12,34));;
+		float a = (float) sum.sum(6.2f, 3);
+		System.out.println(sum2.sum(5, 10, 12, 34));
+		;
 	}
 
 }
---
+++ CoreJava/src/staticKeyword/StaticClassDemo.java
@@ -0,0 +1,14 @@
+package staticKeyword;
+
+//Illegal modifier for the class StaticClass; only public, abstract & final are permitted
+//static class StaticClass{}
+
+
+public class StaticClassDemo {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
